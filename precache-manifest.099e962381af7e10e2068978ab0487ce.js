63f2cff7c96514caa4c6befd781193551c8b8bb6
felipe <feliperarnau@gmail.com>
2020-08-18 16:17:11 -0300

    Updates

---
+++ precache-manifest.099e962381af7e10e2068978ab0487ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9878f1f511c31b3053c76b08cfb40936",
+    "url": "/React-Fetch-Posts-deploy/index.html"
+  },
+  {
+    "revision": "81ecc585bce7d7d7c6eb",
+    "url": "/React-Fetch-Posts-deploy/static/css/main.ec275756.chunk.css"
+  },
+  {
+    "revision": "cf952eb076aadac3a4b6",
+    "url": "/React-Fetch-Posts-deploy/static/js/2.02e2bd51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Fetch-Posts-deploy/static/js/2.02e2bd51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ecc585bce7d7d7c6eb",
+    "url": "/React-Fetch-Posts-deploy/static/js/main.b877a5c2.chunk.js"
+  },
+  {
+    "revision": "07603bc1d4ccfe51b698",
+    "url": "/React-Fetch-Posts-deploy/static/js/runtime-main.f5bce25e.js"
+  }
+]);
